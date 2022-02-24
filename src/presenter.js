2c18b9d3a7ceb7fa949170cc0e2af47e6dec0bda
BMB <bruno.marquez@ucb.edu.bo>
2022-02-24 11:07:00 -0400

    Tercera Funcionalidad

--- src/presenter.js
+++ src/presenter.js
@@ -5,10 +5,13 @@ const div_cantidad_item = document.querySelector("#cantidad-item-div");
 const precio_item = document.querySelector("#precio-item");
 const div_precio_item = document.querySelector("#precio-item-div");
 
+const codigo_estado = document.querySelector("#codigo-estado");
+const div_codigo_estado = document.querySelector("#codigo-item-div");
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   div_cantidad_item.innerHTML = "<p> Cantidad de item: " + cantidad_item.value + "</p>";
   div_precio_item.innerHTML = "<p> Precio de item: " + precio_item.value + "</p>";
-  
+  div_codigo_estado.innerHTML = "<p> Codigo del Estado: " + codigo_estado.value + "</p>";
 });
