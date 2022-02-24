5d7955ad432b2bbeed26ec638b7d6fc0899fa825
BMB <bruno.marquez@ucb.edu.bo>
2022-02-24 10:51:46 -0400

    Segunda funcionalidad

--- src/presenter.js
+++ src/presenter.js
@@ -2,8 +2,13 @@ const cantidad_item = document.querySelector("#cantidad-item");
 const form = document.querySelector("#totalizador-form");
 const div_cantidad_item = document.querySelector("#cantidad-item-div");
 
+const precio_item = document.querySelector("#precio-item");
+const div_precio_item = document.querySelector("#precio-item-div");
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  div_cantidad_item.innerHTML = "<p> Cantidad de item:" + cantidad_item.value + "</p>";
+  div_cantidad_item.innerHTML = "<p> Cantidad de item: " + cantidad_item.value + "</p>";
+  div_precio_item.innerHTML = "<p> Precio de item: " + precio_item.value + "</p>";
+  
 });
